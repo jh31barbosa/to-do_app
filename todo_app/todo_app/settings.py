6c7b9399033546588616c7f244e67897e6730c95
jh31barbosa <vazavult@gmail.com>
2022-10-30 12:46:15 -0300

    change on the settings

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import django_on_heroku
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+#STATICFILES_STORAGE = white
+
+
+
 
 django_on_heroku.settings(locals())
 # Default primary key field type
